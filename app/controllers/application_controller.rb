8055014fd4c44fbaaf8e4989d2ef8a0bd96861f2
Takumi Yamamoto <2311140076g@ed.fuk.kindai.ac.jp>
2025-10-07 13:30:17 +0000

    push to render.com

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  def aaa
+    logger.debug "aaaaaaaaaaaaaaaaaa"
+  end
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
